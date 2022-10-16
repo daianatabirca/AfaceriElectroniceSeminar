eb9ca4d6d3286d78e24b9fdc64b4a0f1f7ad4737
daianatabirca <72124598+daianatabirca@users.noreply.github.com>
2022-10-16 12:45:18 +0300

    Add files via upload
    Models

---
+++ ProiectMaster.Web/Models/Credential.cs
@@ -0,0 +1,9 @@
+﻿namespace ProiectMaster.Web.Models
+{
+    public static class Credential
+    {
+        public static string Email = "contact.interiorsful@gmail.com";
+
+        public static string Password = "icdqgiwmvaomdfxz";
+    }
+}
---
+++ ProiectMaster.Web/Models/MailMessage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace ProiectMaster.Web.Models
+{
+    public class MailMessage
+    {
+        public List<string> To { get; set; }
+
+        public string Subject { get; set; }
+
+        public IFormFile File { get; set; }
+
+        public List<IFormFile> Files { get; set; }
+    }
+}
