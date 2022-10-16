628d3885faae03e815d02912c275392c65744ce4
daianatabirca <72124598+daianatabirca@users.noreply.github.com>
2022-10-16 12:36:20 +0300

    Add files via upload
    Data Models commit

---
+++ ProiectMaster.Models/DTOs/VM/OrderVM.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using ProiectMaster.Models.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProiectMaster.Models.DTOs.VM
+{
+    public class OrderVM
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string CustomerName { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string CustomerPhoneNumber { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string CustomerEmail { get; set; }
+        [Required]
+        [MaxLength(2000)]
+        public string CustomerAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+
+    }
+}
---
+++ ProiectMaster.Models/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using ProiectMaster.Models.DTOs;
+using ProiectMaster.Models.DTOs.VM;
+using System.Collections.Generic;
+
+namespace ProiectMaster.Models.Interfaces
+{
+    public interface IOrderService
+    {
+        IEnumerable<OrderVM> GetAllOrders();
+        OrderVM GetOrder(int id);
+        void AddOrder(OrderVM dto);
+        void UpdateOrder(int id, OrderVM dto);
+        void DeleteOrder(int id);
+    }
+}
