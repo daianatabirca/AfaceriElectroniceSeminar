f5c976a6e7c845b3521b4b190daa0ef5c4ef9a98
daianatabirca <72124598+daianatabirca@users.noreply.github.com>
2022-10-16 12:47:11 +0300

    Add files via upload
    Views folder

---
+++ ProiectMaster.Web/Views/Shared/IViewRenderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectMaster.Web.Views.Shared
+{
+    public interface IViewRenderService
+    {
+        Task<string> RenderToStringAsync(string viewName, object model);
+    }
+}
